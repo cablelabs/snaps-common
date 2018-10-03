55f7b9d811788dbffe23d8018df7599f494200e9
spisarski <s.pisarski@cablelabs.com>
2018-10-03 14:40:40 -0600

    repackaging

--- tests/ansible_snaps/ansible_utils.py
+++ tests/ansible_snaps/ansible_utils.py
@@ -17,7 +17,7 @@ import unittest
 import pkg_resources
 from mock import patch
 
-from snaps_common.provisioning import ansible_utils
+from snaps_common.ansible_snaps import ansible_utils
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -32,7 +32,7 @@ class AnsibleUtilsTests(unittest.TestCase):
 
     def setUp(self):
         self.pb_loc = pkg_resources.resource_filename(
-            'tests.provisioning.playbooks', 'simple_playbook.yaml')
+            'tests.ansible_snaps.playbooks', 'simple_playbook.yaml')
 
     @patch('ansible.executor.playbook_executor.PlaybookExecutor.run',
            return_value=0)
